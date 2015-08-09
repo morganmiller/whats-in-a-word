ed54ebb8a80a2d366f8ff7d5003d2dcb64cb3fa7
Morgan Miller <m.miller722@gmail.com>
2015-08-09 13:10:51 -0600

    fix parser error

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -2,13 +2,13 @@ class Parser
   def self.create_words
     state = State.find_by(name: "CO")
     # State.all.each do |state|
-    Word.find_or_create_by(Parser.word_attrs(state))
+    Word.find_or_create_by(Requester.word_attrs(state))
     # end
   end
 
   def self.create_quotes
     Word.all.each do |word|
-      Quote.find_or_create_by(Parser.quote_attrs(word, word.state))
+      Quote.find_or_create_by(Requester.quote_attrs(word, word.state))
     end
   end
 end
