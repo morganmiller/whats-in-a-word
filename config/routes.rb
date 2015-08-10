341d4ab5c2bfb45bb7df93f99f58de9eae0f97b4
Morgan Miller <m.miller722@gmail.com>
2015-08-09 19:07:56 -0600

    add omniauth routes: pick up at step 5

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: 'states#index'
 
   resources :states, only: [:show, :index]
+
+  get '/auth/twitter', as: 'login'
+  get '/auth/twitter/callback', to: 'sessions#create'
+  get '/logout', to: "sessions#destroy", as: 'logout'
 end
